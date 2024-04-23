6222497541604d2e1bf86cfd5362b4d87a476e6e
Fischmiep <fischmiep@posteo.net>
2024-04-23 11:39:44 +0200

    feat(neorg): Add neorg config

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -11,6 +11,65 @@ return {
   },
   lazy = false,
   version = '*',
-  config = true,
+  config = function()
+    require('neorg').setup {
+      load = {
+        ['core.clipboard.code-blocks'] = {},
+        ['core.completion'] = {
+          config = {
+            engine = 'nvim-cmp',
+          },
+        },
+        ['core.concealer'] = {
+          config = {
+            init_open_folds = 'always',
+          },
+        },
+        ['core.dirman'] = {
+          config = {
+            workspaces = {
+              notes = '~/Documents/Notes',
+              journal = '~/Documents/Journal',
+              paper = '~/Documents/Papers',
+              books = '~/Documents/Books',
+            },
+            default_workspace = 'notes',
+          },
+        },
+        ['core.esupports.hop'] = {},
+        ['core.esupports.indent'] = {},
+        ['core.esupports.metagen'] = {},
+        ['core.export'] = {},
+        ['core.export.markdown'] = {
+          config = {
+            extensions = 'all',
+          },
+        },
+        ['core.integrations.image'] = {},
+        ['core.itero'] = {},
+        ['core.journal'] = {
+          config = {
+            journal_folder = '',
+            workspace = 'journal',
+          },
+        },
+        ['core.keybinds'] = {},
+        ['core.latex.renderer'] = {},
+        ['core.looking-glass'] = {},
+        ['core.pivot'] = {},
+        ['core.presenter'] = {
+          config = {
+            zen_mode = 'zen-mode',
+          },
+        },
+        ['core.promo'] = {},
+        ['core.qol.toc'] = {},
+        ['core.qol.todo_items'] = {},
+        ['core.summary'] = {},
+        ['core.tangle'] = {},
+        ['core.ui.calendar'] = {},
+      },
+    }
+  end,
 }
 -- vim: ts=2 sts=2 sw=2 et
